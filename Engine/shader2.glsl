a7ca928344dd5b26242822575ca59e34696e8a60
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2021-04-18 10:53:28 +0200

    second shader

---
+++ Engine/shader2.glsl
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+#ifdef TEXTURED_GEOMETRY
+
+#if defined(VERTEX) ///////////////////////////////////////////////////
+
+layout(location = 0) in vec3 aPosition;
+layout(location = 2) in vec2 aTexCoord;
+
+out vec2 vTexCoord;
+
+void main()
+{
+	vTexCoord = aTexCoord;
+
+	float clippingScale = 5.0;
+
+	gl_Position = vec4(aPosition, clippingScale);
+	gl_Position.z = -gl_Position.z;
+}
+
+#elif defined(FRAGMENT) ///////////////////////////////////////////////
+
+in vec2 aTexCoord;
+
+uniform sampler2D uTexture;
+
+layout(location = 0) out vec4 oColor;
+
+void main()
+{
+	oColor = texture(uTexture, vTexCoord);
+}
+
+#endif
+#endif
+

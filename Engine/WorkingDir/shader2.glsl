5d1f529f6d5b9bff362c8d741ad9c27c8a35ac75
Oriol Capdevila <36154523+OriolCS2@users.noreply.github.com>
2021-04-20 17:35:49 +0200

    f

--- Engine/WorkingDir/shader2.glsl
+++ Engine/WorkingDir/shader2.glsl
@@ -1,38 +1,36 @@
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-#ifdef TEXTURED_GEOMETRY
+#ifdef SHOW_TEXTURED_MESH
 
 #if defined(VERTEX) ///////////////////////////////////////////////////
 
-layout(location = 0) in vec3 aPosition;
-layout(location = 2) in vec2 aTexCoord;
+layout(location=0) in vec3 aPosition;
+layout(location=2) in vec2 aTexCoord;
 
 out vec2 vTexCoord;
 
-void main()
-{
-	vTexCoord = aTexCoord;
+void main() {
+    vTexCoord = aTexCoord;
 
-	float clippingScale = 5.0;
+    float clippingScale = 5.0;
 
-	gl_Position = vec4(aPosition, clippingScale);
-	gl_Position.z = -gl_Position.z;
+    gl_Position = vec4(aPosition, clippingScale);
+
+    gl_Position.z = -gl_Position.z;
 }
 
 #elif defined(FRAGMENT) ///////////////////////////////////////////////
 
-in vec2 aTexCoord;
+in vec2 vTexCoord;
 
 uniform sampler2D uTexture;
 
 layout(location = 0) out vec4 oColor;
 
-void main()
-{
-	oColor = texture(uTexture, vTexCoord);
+void main() {
+    oColor = texture(uTexture, vTexCoord);
 }
 
 #endif
 #endif
-
